c7c9d5d01d4ea508f624e535c264f945f4255801
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-05-26 23:57:05 +0500

    2nd section responsive

--- src/Pages/Skill/Skill.jsx
+++ src/Pages/Skill/Skill.jsx
@@ -4,12 +4,12 @@ import Card from "../../Components/Card/Card";
 
 const Skill = () => {
   return (
-    <div className="w-full h-auto flex flex-col gap-10 justify-between">
-      <div className="w-[100%] h-auto flex flex-col items-center justify-center gap-3">
+    <div className="w-full h-auto flex flex-col gap-10 justify-center items-center">
+      <div className="w-[90%] h-auto flex flex-col items-start justify-center gap-3">
         <SubHeading text="My Skill" />
-        <h2 className="w-[80%] h-fit font-bold text-4xl">My Expertise</h2>
+        <h2 className="h-fit font-bold text-4xl">My Expertise</h2>
       </div>
-      <div className="cards w-[100%] h-auto flex items-center justify-center gap-3">
+      <div className="cards w-full h-auto flex items-center justify-center gap-3">
         <Card title="Strategy Direction" image=""/>
         <Card title="Branding & Logo" image=""/>
         <Card title="UI/UX Designer" image=""/>
